dcb02c5d372456491902ad1bcb97d17f00d1a453
Andrew Wasef <86547011+AndrewWasef@users.noreply.github.com>
2023-01-29 21:23:47 +0000

    Last Update

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -9,3 +9,39 @@ export TYPEORM_PORT=5532
 export TYPEORM_USERNAME=postgres
 export TYPEORM_PASSWORD=password
 export TYPEORM_DATABASE=glee
+
+
+sudo useradd --no-create-home --system --shell /bin/false prometheus
+sudo mkdir /etc/prometheus
+sudo mkdir /var/lib/prometheus
+wget https://github.com/prometheus/prometheus/releases/download/v2.19.0/prometheus-2.19.0.linux-amd64.tar.gz
+sudo tar xvfz prometheus-2.19.0.linux-amd64.tar.gz
+sudo cp prometheus-2.19.0.linux-amd64/prometheus /usr/local/bin
+sudo cp prometheus-2.19.0.linux-amd64/promtool /usr/local/bin/
+sudo cp -r prometheus-2.19.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-2.19.0.linux-amd64/console_libraries /etc/prometheus
+sudo cp prometheus-2.19.0.linux-amd64/promtool /usr/local/bin/
+sudo rm -rf prometheus-2.19.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
+sudo echo -e "global: \n  scrape_interval: 15s \n  external_labels: \n    monitor: 'prometheus' \n \nscrape_configs: \n  - job_name: 'prometheus' \n    static_configs: \n      - targets: ['localhost:9090']" > /etc/prometheus/prometheus.yml
+sudo echo -e "[Unit] \nDescription=Prometheus \nWants=network-online.target \nAfter=network-online.target \n \n \n[Service] \nUser=prometheus \nGroup=prometheus \nType=simple \nExecStart=/usr/local/bin/prometheus --config.file=/etc/prometheus/prometheus.yml --storage.tsdb.path=/var/lib/prometheus/ --web.console.templates=/etc/prometheus/consoles --web.console.libraries=/etc/prometheus/console_libraries \n\n[Install] \nWantedBy=multi-user.target" > /etc/systemd/system/prometheus.service
+sudo chown prometheus:prometheus /etc/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+sudo chown -R prometheus:prometheus /var/lib/prometheus
+sudo systemctl daemon-reload
+sudo systemctl enable --now prometheus
+
+
+
+curl -X POST http://localhost:9090/-/reload
+sudo apt-get install -y apt-transport-https software-properties-common
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+sudo echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get -y install grafana
+sudo systemctl enable --now grafana-server
+
+
+grafana-cli admin reset-admin-password my new password
