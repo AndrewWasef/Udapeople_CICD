5b1db69ec9d0fec14194264f31ff40587a80dda1
Andrew Wasef <86547011+AndrewWasef@users.noreply.github.com>
2023-01-29 17:06:17 +0000

    ISA

---
+++ backend/set_env.sh
@@ -0,0 +1,11 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_HOST=ud-db.cwmoejujnyqg.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=JvzumuG51gsXru0CZGs3
+export TYPEORM_DATABASE=glee
